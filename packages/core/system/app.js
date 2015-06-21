42603eb12f13cc40549bbdf2592056511da93d17
omirel <olaf@mirel.de>
2015-06-22 00:42:54 +0200

    Template setup

--- packages/core/system/app.js
+++ packages/core/system/app.js
@@ -17,8 +17,30 @@ SystemPackage.register(function(app, auth, database) {
   //We enable routing. By default the Package Object is passed to the routes
   SystemPackage.routes(app, auth, database);
 
-  SystemPackage.aggregateAsset('css', 'common.css');
+  // SystemPackage.aggregateAsset('css', 'common.css');
   SystemPackage.angularDependencies(['ui.router', 'mean-factory-interceptor']);
+
+  // Lib aggregated Files
+  var libDir = '../lib/';
+
+  <!-- CSS Global Compulsory -->
+  SystemPackage.aggregateAsset('css', libDir + 'plugins/bootstrap/css/bootstrap.min.css');
+  SystemPackage.aggregateAsset('css', libDir + 'css/style.css');
+
+  <!-- CSS Header and Footer -->
+  SystemPackage.aggregateAsset('css', libDir + 'css/headers/header-default.css');
+  SystemPackage.aggregateAsset('css', libDir + 'css/footers/footer-v1.css');
+
+  <!-- CSS Implementing Plugins -->
+  SystemPackage.aggregateAsset('css', libDir + 'plugins/animate.css');
+  SystemPackage.aggregateAsset('css', libDir + 'plugins/line-icons/line-icons.css');
+  SystemPackage.aggregateAsset('css', libDir + 'plugins/font-awesome/css/font-awesome.min.css');
+
+  <!-- CSS Page Style -->
+  SystemPackage.aggregateAsset('css', libDir + 'css/pages/page_about.css');
+
+  <!-- CSS Customization -->
+  SystemPackage.aggregateAsset('css', libDir + 'css/custom.css');
 
   // The middleware in config/express will run before this code
 
